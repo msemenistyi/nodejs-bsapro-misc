8469ea2449407e21383978d3a9abf34db86deb1c
Mykyta Semenistyi <nikeiwe@gmail.com>
2017-02-01 23:51:48 +0200

    loopback: add user entity

--- backend/loopback/server/boot/root.js
+++ backend/loopback/server/boot/root.js
@@ -3,6 +3,8 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res, next) => {
+  	res.render('users');
+  });
   server.use(router);
 };
